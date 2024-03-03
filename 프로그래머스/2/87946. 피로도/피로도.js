2fbe4b5f5273bc37ba8ce3aad3b7852656045222
okimyj <okimyj@gmail.com>
2024-03-03 16:42:59 +0900

    [level 2] Title: 피로도, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,31 +1,28 @@
 function solution(k, dungeons) {
     var answer = -1;
     let max = 0;
-    const DFS = (hp, index, visited, depth) =>{
-        if(hp < dungeons[index][0]){
-            // console.log("cannot more - depth : ", depth, " hp : ", hp, " dungeon : ", dungeons[index])
-            answer = Math.max(visited.size, answer)
-            return;
-        }
-        visited.add(index+1);
-        if(depth === 1){
-            answer = Math.max(visited.size, answer)
-            return;
-        }
-        // console.log("DFS - index : ", index+1, "hp : ", hp, " data : ", dungeons[index] , " depth : ", depth, " visited : ", visited)
+    const DFS = (hp, index, visited) =>{
+        visited.add(index);
+        answer = Math.max(answer, visited.size);
+        hp -= dungeons[index][1];
+        // console.log("hp : " , hp, " index: ", index, " visited : " , visited, " info : ", dungeons[index] )
         for(let i =0; i<dungeons.length; ++i){
-            if(!visited.has(i+1)){
-                DFS(hp - dungeons[index][1], i, visited, depth-1);
-                visited.delete(i+1);
+            // console.log("inner for - index : ", index, " hp : ", hp)
+            if(!visited.has(i) && hp >= dungeons[i][0]){
+                DFS(hp, i, visited);
+                // console.log("-- inner DFS out --")
+                visited.delete(i);
             }
         }
     }
-    
+    const visited = new Set();
     for(let i =0; i<dungeons.length; ++i){
-        
-        DFS(k, i, new Set(), dungeons.length);
-        // const num = DFS(k, i, new Set(), dungeons.length);
-        // answer = Math.max(answer, num);
+        if(k >= dungeons[i][0]){
+            // console.log("- - start DFS - - start index : ", i )
+            DFS(k, i, visited);
+            visited.clear();
+        }
+
     }
     return answer;
 }
