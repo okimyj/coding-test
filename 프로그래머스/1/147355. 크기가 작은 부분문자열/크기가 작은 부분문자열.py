246d4164bf0763f2e9e5d9914dae9a0cfdca0821
okimyj <okimyj@gmail.com>
2024-03-13 17:11:53 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.09 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -2,7 +2,9 @@ def solution(t, p):
     answer = 0
     pLen = len(p)
     num_p = int(p)
+    # for(let i =0; i<=t.length - p.length; ++i)
     for i in range(len(t)-pLen+1) : 
+        print("i : " , i , " t[i]:", t[i])
         split_num = int(t[i:i+pLen])
         if split_num <= num_p:
             answer+=1
