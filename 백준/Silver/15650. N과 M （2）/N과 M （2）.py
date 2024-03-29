918b304766372a386c0cf58ad781982ff91f5549
okimyj <okimyj@gmail.com>
2024-03-29 09:52:14 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,10 +1,11 @@
 import sys
-N, M = map(int, sys.stdin.readline().rstrip().split(' '))
-arr =[]
+N, M = map(int, sys.stdin.readline().rstrip().split())
+arr = []
 def dfs(start) :
     if len(arr) == M :
-        print(' '.join(map(str, arr)))
+        print(*arr)
         return
+
     for i in range(start, N+1) :
         if i not in arr :
             arr.append(i)
