4883354a2abfe05edabf0be165fb65f4c2584c73
okimyj <okimyj@gmail.com>
2024-03-29 12:50:22 +0900

    [Silver III] Title: 바이러스, Time: 60 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -7,19 +7,13 @@ for _ in range(int(input())) :
     s, e = map(int, input().rstrip().split(' '))
     graph[s].append(e)
     graph[e].append(s)
-
-# def dfs(cur=startV, visited=[]) :
-#     visited.append(cur)
-#     for v in graph[cur] :
-#         if v not in visited :
-#             visited = dfs(v, visited)
-#     return visited
-
-def dfs(startV, visited = []) :
+    
+visited=[]
+def dfs(startV) :
     visited.append(startV)
     for v in graph[startV] :
         if v not in visited :
-            visited = dfs(v, visited)
-    return visited
+            dfs(v)
+    return len(visited)-1
 
-print(len(dfs(1))-1)
+print(dfs(1))
