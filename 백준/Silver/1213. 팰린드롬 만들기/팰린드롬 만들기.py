107f362f1b1c4f666737dc1a76182a9a4dd6fd77
okimyj <okimyj@gmail.com>
2024-03-30 10:41:47 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,29 +1,26 @@
+# 4일차 - 5번 팰린드롬 만들기
+# I'm Sorry Hansoo
 import sys
 from collections import defaultdict
 wtf = defaultdict(int)
 for c in sys.stdin.readline().rstrip() :
     wtf[c] += 1
 # dict sort
-# wtf =  sorted(wtf.items(), key = lambda x : (-x[1], x[0]))
 wtf =  sorted(wtf.items())
 def make() :
-    # 홀수인게 하나 이상이면 안 됨.
-    # 가운데 하나만 들어올 수 있으니까.... 무조건 알파벳 순으로 정렬해도 안되네?
+    # 홀수인게 하나 이상이면 안 됨. 가운데 하나만 들어올 수 있으니까....
     oddChar = ''
-    answer = ""
+    answer = ''
     for c in wtf :
         if c[1] % 2 != 0 :
             if oddChar != '' :
-              print("I'm Sorry Hansoo")
-              return
+              return "I'm Sorry Hansoo"
             oddChar = c[0]
             if c[1] > 1 :
                 answer += c[0] * (c[1] // 2)
         else :
             answer += c[0] * (c[1]//2)
 
-    answer = answer + oddChar + answer[::-1]
+    return(answer + oddChar + answer[::-1])
 
-    print(answer)
-
-make()
+print(make())
