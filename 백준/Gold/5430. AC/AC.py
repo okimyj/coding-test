66f03298c21729a6aeffab786c43c2bd80403ede
okimyj <okimyj@gmail.com>
2024-04-03 15:22:09 +0900

    [Gold V] Title: AC, Time: 300 ms, Memory: 41352 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,45 @@
+# AC
+# R - 배열에 있는 수의 순서를 뒤집음 [1, 2, 3, 4] -> [4, 3, 2, 1]
+# D - 배열의 첫번째 인덱스를 버림
+# 그냥 리스트 reverse, pop(0) 사용했더니 시간초과.
+# deque 사용 시도.
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def D(q, isPopLeft):
+    if len(q) <= 0 :
+        return False
+    if isPopLeft :
+        q.popleft()
+    else :
+        q.pop()
+    return True
+
+# 첫 번째 입력 테스트 케이스 만큼 반복.
+for i in range(int(input())) :
+    # 연속으로 RR 두번 나오는건 어차피 제자리..
+    # operations = list(input().rstrip().replace('RR', ''))
+    operations = list(input().rstrip())
+    input() # 입력 값 개수
+    strNumbers = input().rstrip()[1:-1]
+    # numbers = len(strNumbers) > 0 and list(map(int, strNumbers.split(','))) or []
+    numbers = deque(len(strNumbers) > 0 and list(map(int, strNumbers.split(','))) or [])
+    isPass = len(operations) <= 0
+    isPopLeft = True
+    for op in operations :
+        if op == 'R' :
+            isPopLeft = not isPopLeft
+            isPass = True
+        elif op == 'D' :
+            isPass = D(numbers, isPopLeft)
+        if not isPass :
+            break
+
+    if isPass :
+        if not isPopLeft :
+            numbers.reverse()
+        print('[{}]'.format(','.join(str(v) for v in numbers)))
+        # print(','.join(numbers))
+    else:
+        print('error')
