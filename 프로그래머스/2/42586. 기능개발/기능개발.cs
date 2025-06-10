e303e61b05a303c865cc3fdb49a4e7d8fc95ea7c
Kim Yajin <okimyj@gmail.com>
2025-06-10 13:01:34 +0900

    [level 2] Title: 기능개발, Time: 2.34 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cs
+++ 프로그래머스/2/42586. 기능개발/기능개발.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 public class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         List<int> answer = new List<int>();
-        List<int> workDays = new List<int>();
+        /*List<int> workDays = new List<int>();
         for(int i=0; i<progresses.Length; ++i)
         {
             workDays.Add((int)Math.Ceiling((100f - progresses[i]) / speeds[i]));
         }
+        */
+        List<int> workDays = progresses.Select((value, i)=>(int)Math.Ceiling((100f- value)/speeds[i])).ToList();
+        
         int prevMaxDeployDay = workDays[0];
         int deployNumOfDay = 0;
         for(int i=0; i<workDays.Count; ++i)
