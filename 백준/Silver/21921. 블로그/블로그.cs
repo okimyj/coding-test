4d8850c7400a6e6561d717544ad59ebf00dd0086
Kim Yajin <okimyj@gmail.com>
2025-07-16 20:37:17 +0900

    [Silver III] Title: 블로그, Time: 132 ms, Memory: 21112 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.cs
+++ 백준/Silver/21921. 블로그/블로그.cs
@@ -7,30 +7,37 @@ namespace CodingTest
         static void Main(string[] args)
         {
             int[] input = Console.ReadLine().Split(" ").Select(c => Convert.ToInt32(c)).ToArray();
+            int N = input[0];
             int X = input[1];
             input = Console.ReadLine().Split(" ").Select(c => Convert.ToInt32(c)).ToArray();
-            int left = 0;
-            int max = 0;
+                        
             int sum = 0;
-            List<int> visitorSum = new List<int>();
-            for(int right=0; right < input.Length; right++)
+            for(int i=0; i<X; i++)
             {
-                if (right - left >= X)
-                {
-                    visitorSum.Add(sum);
-                    left = left + 1;
-                    sum -= input[right - X];
-                }
-                sum += input[right];
+                sum += input[i];
             }
-            visitorSum.Add(sum);
-            List<int> sorted = visitorSum.OrderByDescending(x => x).ToList();
-            if (sorted[0] == 0)
+            int max = sum;
+            int count = 1;
+            for (int right=X; right < N; right++)
+            {
+                sum += input[right] - input[right-X];
+                if (max == sum)
+                {
+                    ++count;
+                }
+                else if (max < sum)
+                {
+                    max = sum;
+                    count = 1;
+                }
+            }
+
+            if (max == 0)
                 Console.WriteLine("SAD");
             else
             {
-                Console.WriteLine(sorted[0]);
-                Console.WriteLine(sorted.Count(v => v == sorted[0]));
+                Console.WriteLine(max);
+                Console.WriteLine(count);
             }
         }
     }
