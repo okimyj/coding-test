cdd8ed456d894a691e49ee9ecd453df2e6960e86
Kim Yajin <okimyj@gmail.com>
2025-08-26 11:41:40 +0900

    [Silver IV] Title: 괄호, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cs
+++ 백준/Silver/9012. 괄호/괄호.cs
@@ -1,36 +1,41 @@
-namespace OKIMYJ
+namespace BaekJoon
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
-			int lineNum = int.Parse(Console.ReadLine());
-			for (int i = 0; i < lineNum; ++i)
-			{
-				string brackets = Console.ReadLine();
-				Stack<char> check = new Stack<char>();
-				foreach (char c in brackets)
-				{
-					if (c == '(')
-						check.Push(c);
-					else
-					{
-						if (check.Count() > 0 && check.Last() == '(')
-						{
-							check.Pop();
-						}
-						else
-						{
-							check.Push(c);
-							break;
-						}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new BaekJoon_9012().Start();
+        }
+    }
+    public class BaekJoon_9012
+    {
+        public void Start()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int i=0; i<N; i++)
+            {
+                string input = Console.ReadLine();
+                for(int c = 0; c < input.Length; c++)
+                {
+                    if (input[c] == '(')
+                    {
+                        ++count;
+                    }
+                    else
+                    {
+                        --count;
+                        if (count < 0)
+                            break;
+                    }
+                }
+                if (count == 0)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+                count = 0;
+            }
+        }
+    }
 
-					}
-				}
-
-				Console.WriteLine(check.Count() > 0 ? "NO" : "YES");
-			}
-
-		}
-	}
 }
